a07386638f74f33efc429bba9882e8534cc9ac4f
Chetanya Chonkar <chonkarchetanya@gmail.com>
2025-08-21 15:45:41 +0530

    Adding new content

---
+++ index.js
@@ -0,0 +1,17 @@
+//Node.js
+
+//JavaScript Runtime Environment
+//It is used for server side programming.
+
+
+//* Node.js is not a language, library or framework.
+
+
+// Node REPL
+//Read, Evaluate, Print and Loop
+
+// .help gives us commands
+
+
+// Node Files 
+// node fileName.js 
