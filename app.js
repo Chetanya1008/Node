fcf99ec3df04a0223d9309b704ef9ccb28b6a0fe
Chetanya Chonkar <chonkarchetanya@gmail.com>
2025-08-22 08:16:29 +0530

    Added more node content

---
+++ app.js
@@ -0,0 +1,32 @@
+//Node.js
+
+//JavaScript Runtime Environment
+//It is used for server side programming.
+
+
+//* Node.js is not a language, library or framework.
+
+
+// Node REPL
+//Read, Evaluate, Print and Loop
+
+// .help gives us commands
+
+
+// Node Files 
+// node fileName.js 
+
+
+//Process 
+// process: This object provides information about, and control over, the current Node.js process.
+
+// process.argv: returns an array containing the command-line arguments passed when the Node.js process was launched.
+
+
+//moduel.exports 
+//requiring files
+
+// require() : a built-in function to include external modules that exist in separate files.
+
+// module.exports : a special object
+
